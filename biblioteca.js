60c622ea999290a5c00767248a5171ea59bb4748
Gabriel979 <gabrielsanchez.frh@gmail.com>
2016-01-24 09:58:55 -0300

    Terminado Orientacion del viento

--- biblioteca.js
+++ biblioteca.js
@@ -111,8 +111,8 @@ function mostrarClima(latitud,longitud){
         $("#currently-temperature").html("Temperatura: " + clima.currently.temperature + " ºC");
         $("#currently-apparentTemperature").html("Sensación Térmica: " + clima.currently.apparentTemperature + " ºC");
         $("#currently-windSpeed").html("Velocidad del Viento: " + clima.currently.windSpeed + " km/h");
-        $("#currently-windBearing").html("Orientación del Viento: " + clima.currently.windBearing + "º :: " + orientacion(clima.currently.windBearing));
-        $("#currently-humidity").html("Humedad: " + clima.currently.humidity*100 +"%");
+        $("#currently-windBearing").html("Orientación del Viento: " + orientacion(clima.currently.windBearing));
+        $("#currently-humidity").html("Humedad: " + clima.currently.humidity*100 +" %");
         $("#currently-dewPoint").html("Punto de Rocio: " + clima.currently.dewPoint + " ºC");
         $("#currently-pressure").html("Presión: " + clima.currently.pressure + " hPa");
         
@@ -128,66 +128,102 @@ function mostrarClima(latitud,longitud){
     function orientacion(grados){
 
 
-        if((grados==0 && grados<=11)|| grados==360)
+        if((grados>=0 && grados<6)|| (grados>354 && grados<=360))
             respuesta="N (Norte)";
 
-        if(grados>11 && grados<=22)
+        if(grados>=6 && grados<17)
             respuesta="NpE (Norte por Este)";
 
-        if(grados>22 && grados<34)
+        if(grados>=17 && grados<=28)
             respuesta="NNE (Nornoroeste)";
 
-        if(grados>=34 && grados<45)
+        if(grados>28 && grados<=39)
             respuesta="NEpN (Noreste por Norte)";
 
-        if(grados>=45 && grados<=56)
+        if(grados>39 && grados<51)
             respuesta="NE (Noreste)";
         
-        if(grados>56 && grados<=67)
+        if(grados>=51 && grados<62)
             respuesta="NEpE (Noreste por Este)";
 
-        if(grados>67 && grados<79)
+        if(grados>=62 && grados<=73)
             respuesta="ENE (Estenoreste)";
                         
-        if(grados>=79 && grados<90)
+        if(grados>73 && grados<=84)
             respuesta="EpN (Este por Norte)";
                             
-        if(grados>=90 && grados<=101)
+        if(grados>84 && grados<96)
             respuesta="E (Este)";
 
-        if(grados>101 && grados<=112)
+        if(grados>=96 && grados<107)
             respuesta="EpS (Este por sur)";
 
-        if(grados>112 && grados<124)
+        if(grados>=107 && grados<=118)
             respuesta="ESE (Estesureste)";
 
-        if(grados>=124 && grados<135)
+        if(grados>118 && grados<=129)
             respuesta="SEpE (Sureste por Este)";
 
-        if(grados>=135 && grados<=146)
+        if(grados>129 && grados<141)
             respuesta="SE (Sureste)";
 
-        if(grados>146 && grados<=157)
+        if(grados>=141 && grados<150)
             respuesta="SEpS (Sureste por Sur)";
 
-        if(grados>157 && grados<169)
+        if(grados>=150 && grados<=163)
             respuesta="SSE (Sursurste)";
 
-        if(grados>=169 && grados<180)
+        if(grados>163 && grados<=174)
             respuesta="SpE (Sur por Este)";
 
-        if(grados>=180 && grados<=191)
+        if(grados>174 && grados<186)
             respuesta="S (Sur)";
 
-        if(grados>=169 && grados<180)
+        if(grados>=186 && grados<197)
             respuesta="SpO (Sur por Oeste)";
 
-        if(grados>=169 && grados<180)
-            respuesta="SpE (Sur por Este)";
+        if(grados>=197 && grados<=208)
+            respuesta="SSO (Sursuroste)";
 
-        if(grados>=169 && grados<180)
-            respuesta="SpE (Sur por Este)";
+        if(grados>208 && grados<=219)
+            respuesta="SOpS (Suroeste por Sur)";
         
+        if(grados>219 && grados<231)
+            respuesta="SOpS (Suroeste)";
+        
+        if(grados>=231 && grados<242)
+            respuesta="SOpO (Suroeste por Oeste)";
+        
+        if(grados>=242 && grados<=253)
+            respuesta="OSO (Oestesuroeste)";
+        
+        if(grados>253 && grados<=264)
+            respuesta="OpS (Oeste por Sur)";
+        
+        if(grados>264 && grados<276)
+            respuesta="O (Oeste)";
+        
+        if(grados>=276 && grados<287)
+            respuesta="OpN (Oeste por Norte)";
+
+        if(grados>=287 && grados<=298)
+            respuesta="ONO (Oestenoroeste)";
+
+        if(grados>298 && grados<=309)
+            respuesta="NOpO (Noroeste por Oeste)";
+
+        if(grados>309 && grados<321)
+            respuesta="NO (Noroeste)";
+        
+        if(grados>=321 && grados<332)
+            respuesta="NOpN (Noroeste por Norte)";
+
+        if(grados>=332 && grados<=343)
+            respuesta="NNO (Nornoroeste)";
+
+        if(grados>343 && grados<=354)
+            respuesta="NNO (Norte por Oeste)";
+
         if(grados<0 || grados>360)
             respuesta="Error de Coordenadas!";
 
